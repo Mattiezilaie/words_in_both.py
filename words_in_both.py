2f53fa8646f7b44d5703ca6cc2e492f862b49639
Mattie Zilaie <your_name@domain.comAccount>
2019-11-19 17:19:34 -0800

    function that takes  two string parameters and returns a set of words in both(in lower case)

---
+++ words_in_both.py
@@ -0,0 +1,13 @@
+# Author: Mahtab Zilaie
+# Date: November 19 2019
+# Description: A function that takes two string parameters and returns a set
+# of words in both strings in lower case
+
+def word_in_both(s1,s2):
+    """takes two string parameters and returns a set of the
+    words (in lower case)contained in both strings
+    -capitalization doesn't matter"""
+    words1 = s1.lower().split(" ")
+    words2 = s2.lower().split(" ")
+    return {i for i in words1 if i in words2}
+
